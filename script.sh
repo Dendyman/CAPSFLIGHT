d8b24afb95f2a7a19c1820fe7f37f9433fe7c240
Denis Afanasev <denis.afanasev@sap.com>
2023-07-12 11:13:06 +0000

    comm 2

--- script.sh
+++ script.sh
@@ -1,8 +1,12 @@
 cf login --sso
 
+https://api.cf.us10.hana.ondemand.com
+
 cds deploy --to hana
 
 cds watch --profile hybrid
 
 build:
 mbt build
+
+cf deploy mta_archives/capire.sflight_1.0.0.mtar
